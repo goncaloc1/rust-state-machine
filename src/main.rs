b35de5847fec7e26c840c3655803ae59e08a2ccc
goncaloc1 <goncacosta@gmail.com>
2024-12-30 16:48:29 +0000

    feat: use Runtime

--- src/main.rs
+++ src/main.rs
@@ -19,5 +19,31 @@ impl Runtime {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut runtime = Runtime::new();
+
+    let alice = String::from("alice");
+    let bob = String::from("bob");
+    let charlie = String::from("charlie");
+
+    runtime.balances.set_balance(&alice, 100);
+
+    // start emulating a block
+    runtime.system.inc_block_number();
+    assert_eq!(runtime.system.block_number(), 1);
+
+    // first transaction
+    runtime.system.inc_nonce(&alice);
+
+    let _ = runtime
+        .balances
+        .transfer(alice.clone(), bob.clone(), 30)
+        .map_err(|e| eprintln!("{}", e));
+
+    // second transaction
+    runtime.system.inc_nonce(&alice);
+
+    let _ = runtime
+        .balances
+        .transfer(alice.clone(), charlie.clone(), 20)
+        .map_err(|e| eprintln!("{}", e));
 }
